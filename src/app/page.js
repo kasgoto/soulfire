fcfcf30ab5543d1e7f90271a8b4fb8103e5788bd
kas.goto <meiliunasvilius@gmail.com>
2025-09-22 15:39:37 +0400

    homepage art of becoming section

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Home() {
   return (
@@ -43,11 +44,11 @@ export default function Home() {
       </div>
       {/* Regular Content Section */}
       <div className='bg-white text-black px-8 py-24 lg:px-32'>
-        <h2 className='text-4xl lg:text-5xl font-bold text-center mb-16'>
+        <h2 className='faustina-title text-4xl lg:text-5xl text-center mb-16 font-normal'>
           The Art of Becoming
         </h2>
         {/* Corrected Layout: Centered container with max-width */}
-        <div className='flex flex-col items-center md:flex-row md:items-center md:justify-center md:gap-8 max-w-7xl mx-auto'>
+        <div className='flex flex-col items-center md:flex-row md:items-center md:justify-center md:gap-12 max-w-7xl mx-auto'>
           <Image
             src='/homepageintro.jpeg'
             alt='introduction image'
@@ -57,7 +58,7 @@ export default function Home() {
             priority
           />
           <div className='flex flex-col gap-8 md:w-5/8'>
-            <p className='text-2xl md:text-2xl lg:text-2xl xl:text-3xl'>
+            <p className='text-2xl md:text-2xl lg:text-2xl xl:text-3xl font-extralight'>
               The journey back to your eternal flame begins here. <br /> <br />
               SoulFire Ceremony is the living dance of life itself, a continuous
               practice of restoring harmony to every element of your existence.
@@ -66,20 +67,46 @@ export default function Home() {
               current of Kundalini energy—to guide you toward a deeper mastery
               of consciousness.
             </p>
+            <Link
+              href='/about'
+              className='bg-black text-xl text-white border border-gray-300 py-2 px-6 rounded-full transition-opacity duration-200 hover:opacity-60 max-w:[200px] text-center self-start mt-4'
+            >
+              Learn more
+            </Link>
           </div>
         </div>
       </div>
-      <p className='text-lg mb-8'>
-        Through our gentle and profound practices, we offer you a path home.
-        Explore a personalized <strong>energy analysis</strong> to understand
-        your unique flow, embrace the transformative power of{" "}
-        <strong>Kundalini</strong>, or find deep healing and connection with our
-        specialized <strong>tantric massages</strong>.
-      </p>
-      <p className='text-lg'>
-        This is a ceremony of remembrance, a humble service to guide you back to
-        who you are—your home.
-      </p>
+      <div className='px-8 py-24 lg:px-32 bg-white text-black mx-auto '>
+        <h2 className='faustina-title text-4xl lg:text-5xl text-center mb-16 font-normal'>
+          What we offer
+        </h2>
+        <p className='mb-8 text-2xl md:text-2xl lg:text-2xl xl:text-3xl font-extralight text-center'>
+          Through our gentle and profound practices, we offer you a path home.
+        </p>
+
+        <div className='flex flex-col items-center md:flex-row md:items-center md:justify-center md:gap-12 max-w-7xl mx-auto'>
+          <p className='mb-8 text-2xl md:text-2xl lg:text-2xl xl:text-3xl font-extralight text-center'>
+            Explore a personalized energy analysis to understand your unique
+            flow.
+          </p>
+        </div>
+        <div className='flex flex-col items-center md:flex-row md:items-center md:justify-center md:gap-12 max-w-7xl mx-auto'>
+          <p className='mb-8 text-2xl md:text-2xl lg:text-2xl xl:text-3xl font-extralight text-center'>
+            embrace the transformative power of <strong>Kundalini</strong>
+          </p>
+        </div>
+        <div className='flex flex-col items-center md:flex-row md:items-center md:justify-center md:gap-12 max-w-7xl mx-auto'>
+          <p className='mb-8 text-2xl md:text-2xl lg:text-2xl xl:text-3xl font-extralight text-center'>
+            find deep healing and connection with our specialized tantric
+            massages.
+          </p>
+        </div>
+
+        <p className='text-2xl md:text-2xl lg:text-2xl xl:text-3xl font-extralight'>
+          This is a ceremony of remembrance, a humble service to guide you back
+          to who you are—your home.
+        </p>
+      </div>
     </div>
   )
 }
